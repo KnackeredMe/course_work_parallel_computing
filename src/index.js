2de9cc2cb88d68c31efb6d6d9f86b2e3ed2feb7b
Vadym Marko <marko.vadym@gmail.com>
2022-12-08 19:36:46 +0200

    MINOR: local indexes are merging all at once now

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const {Worker, isMainThread, parentPort, workerData,} = require('worker_threads'
 const fs = require("fs");
 
 const invertedIndex = {};
-const numberOfThreads = 4;
+const localIndexes = [];
+const numberOfThreads = 1;
 let workersFinished = 0;
 
 const dirPath = "./datasets/unsup";
@@ -10,32 +11,35 @@ let fileNames;
 
 fs.readdir(dirPath, (err, files) => {
     fileNames = files;
+    const leftover = fileNames.length % numberOfThreads;
+    let filesPerThread = (fileNames.length - leftover) / numberOfThreads;
     console.time('Total');
     for(let i = 0; i < numberOfThreads; i++) {
-        const leftover = fileNames.length % numberOfThreads;
-        let filesPerThread = (fileNames.length - leftover) / numberOfThreads;
         if (i < leftover) { filesPerThread += 1 }
         const fileNamesForThread = fileNames.slice(i * filesPerThread, (i + 1) * filesPerThread);
         console.time(`Thread ${i + 1}`);
         createWorker(fileNamesForThread).then(result => {
             workersFinished += 1;
-            mergeIndex(result);
             console.timeEnd(`Thread ${i + 1}`);
+            localIndexes.push(result)
             if (workersFinished === numberOfThreads) {
+                mergeIndexes();
                 console.timeEnd('Total');
             }
         })
     }
 })
 
-function mergeIndex(localIndex) {
-    for(const entry of Object.entries(localIndex)) {
-        if (invertedIndex[entry.key]) {
-            invertedIndex[entry.key].push(localIndex[entry.key]);
-            return;
+function mergeIndexes() {
+    localIndexes.forEach(localIndex => {
+        for(const entry of Object.entries(localIndex)) {
+            if (invertedIndex[entry.key]) {
+                invertedIndex[entry.key].push(localIndex[entry.key]);
+                return;
+            }
+            invertedIndex[entry.key] = localIndex[entry.key];
         }
-        invertedIndex[entry.key] = localIndex[entry.key];
-    }
+    })
 }
 
 function createWorker(fileNamesForThread) {
